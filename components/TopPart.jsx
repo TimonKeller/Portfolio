09f36eb24c6b5b278f88ae0822632c882dd8cc9f
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-23 01:21:19 +0200

    Update TopPart.jsx

--- components/TopPart.jsx
+++ components/TopPart.jsx
@@ -6,7 +6,7 @@ const TopPart = () => {
     <div className='container px-16 md:py-16 md:flex md:flex-row gap-x-8 items-center'>
         <div className='flex flex-col gap-y-4 tex-center'>
             <a className='text-center md:text-start font-caveat text-2xl text-text'> {"Hi, I'm Timon Keller"} </a>
-            <p className='text-center md:text-start text-5xl md:text-6xl text-text'>Creating experiences, building digital products </p>
+            <p className='text-center md:text-start text-4xl md:text-6xl text-text'>Creating experiences, building digital products </p>
             <a href="mailto:timon.keller@bluewin.ch" className='self-center md:self-start'>
                 <button className='font-caveat text-text font-bold text-2xl mt-6 border-2 rounded-full p-4 hover:bg-white hover:text-black'>
                     Contact me
