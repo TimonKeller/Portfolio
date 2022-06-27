4de16bc1420a5cf5a56f24c9dcb3dd1df3da48e6
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-27 16:15:53 +0200

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -38,79 +38,8 @@ const Header = () => {
                 </button>
               </a>
             </div>
-            <div className="md:hidden flex">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-none inline-flex items-center justify-center rounded-md text-text"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http:www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http:www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
           </div>
         </div>
-        <Transition
-          show={isOpen}
-          enter="transition ease-out duration-100 transform"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="transition ease-in duration-75 transform"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          {(ref) => (
-            <div className="md:hidden id=mobile-menu flex justify-center">
-              <div
-                ref={ref}
-                className="text-2xl bg-background px-2 pt-2 pb-3 space-y-1 sm:px-3 flex flex-col gap-y-6 justify-center align-center cursor-pointer font-bold text-gray-400 hover:text-text min-h-screen"
-              >
-                <Link href="#about" smooth>
-                  <a onClick={() => setIsOpen(!isOpen)}> About me</a>
-                </Link>
-                <Link href="#projects" smooth>
-                  <a onClick={() => setIsOpen(!isOpen)}>Projects</a>
-                </Link>
-                <Link href="#contact" smooth>
-                  <a onClick={() => setIsOpen(!isOpen)}>Contact</a>
-                </Link>
-              </div>
-            </div>
-          )}
-        </Transition>
       </div>
     </div>
   );
