2dfa4b0288dea319063ef732ce40b1565fb32ee1
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-27 14:34:24 +0200

    Update Projects.jsx

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,7 +4,7 @@ import Tilt from "react-vanilla-tilt";
 
 const Projects = () => {
   return (
-    <div className="container mt-24 md:mt-48 lg:px-24 px-8" id="projects">
+    <div className="container mt-24 md:mt-48 px-8" id="projects">
       <div className="text-center text-6xl text-text">My recent Work</div>
       <div className="md:px-0 py-8 md:py-16 grid md:grid-cols-12 mt-12 gap-x-16">
         <div className="lg:col-span-5 text-center">
