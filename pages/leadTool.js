3324d011480f86fff501b75764d8b0823f9e2222
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-29 01:17:58 +0200

    updates

--- pages/leadTool.js
+++ pages/leadTool.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const leadTool = () => {
   return (
-    <div>leadTool</div>
+    <div className='container mx-auto mb-8'>
+      <div className='flex flex-col shadow-md shadow-black rounded-md'>
+        {<img src='/leadTool_cropped.png' className='rounded-md self-start'/>}
+        <div className='px-20 mt-20'>
+          <h1 className=' text-center md:text-start font-caveat text-blue text-5xl md:text-6xl mb-8'>Lead Tool</h1>
+          <p className='text-center md:text-start text-text text-xl mb-4 '>LeadTool is a CRUD web application I was allowed to develop for Allianz Suisse in my third and fourth semester at the FHNW with fellow students.</p>
+        </div>
+         
+       
+
+      </div>
+    </div>
   )
 }
 
