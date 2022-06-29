711d35c28a18f7cc702a696ceb4e50927b536a72
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-29 23:49:49 +0200

    Update AboutMe.jsx

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -32,11 +32,11 @@ const AboutMe = () => {
           looking forward to getting practical experience and learning new
           things.
         </span>
-        <Link href="/CV.pdf" download target="_blank">
+        <a href="/CV.pdf" download target="_blank">
           <button className="font-caveat text-text font-bold text-2xl mt-6 border-2 rounded-md p-4 hover:bg-blue hover:border-blue self-center md:self-start hover:-translate-y-2 hover:duration-200 hover:ease-in">
             Download CV
           </button>
-        </Link>
+        </a>
       </div>
     </div>
   );
