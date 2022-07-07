d5ce8c4f415bd4de2548abd987d7362823751fd3
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-07-07 20:02:41 +0200

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 import { AboutMe, Layout, Projects, TopPart } from '../components'
-import Script from 'next/script';
+
 
 
 export default function Home() {
